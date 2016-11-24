1b0e141fdab0937cdf830885ea1c3c252c699a82
EddieCCCCC <xjsdal@126.com>
2016-11-24 16:31:28 +0800

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebJobTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescriptiaaaaaaaaaaaaaaon("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebJobTest")]
